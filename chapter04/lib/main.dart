182344d9b4b1f809312c0c02e31fc6a826afae13
naok-000 <wt.94p.1978@s.thers.ac.jp>
2025-03-24 16:47:28 +0900

    電話帳アプリのUIを作成

--- chapter04/lib/main.dart
+++ chapter04/lib/main.dart
@@ -37,45 +37,37 @@ class _MyHomePageState extends State<MyHomePage> {
 
         title: Text(widget.title),
       ),
-      body: Center(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            ElevatedButton(
-              onPressed: () {
-                showDialog(
-                  context: context,
-                  builder: (context) {
-                    return AlertDialog(title: Text("ログインしました"));
-                  },
-                );
-              },
-              child: Text("ログインする"),
+      body: ListView(
+        children: [
+          ListTile(
+            // leading: FlutterLogo(size: 72.0),
+            leading: Icon(Icons.phone),
+            title: Text('山田 太郎'),
+            subtitle: Text(
+              '070-1234-567',
             ),
-            ElevatedButton(
-              onPressed: () {
-                showDialog(
-                  context: context,
-                  builder: (context) {
-                    return AlertDialog(title: Text("新規登録しました"));
-                  },
-                );
-              },
-              child: Text("新規登録をする"),
+            trailing: Icon(Icons.keyboard_arrow_right),
+            onTap: (){},
+          ),ListTile(
+            // leading: FlutterLogo(size: 72.0),
+            leading: Icon(Icons.phone),
+            title: Text('鈴木 一郎'),
+            subtitle: Text(
+              '080-1234-567',
             ),
-            TextButton(
-              onPressed: () {
-                showDialog(
-                  context: context,
-                  builder: (context) {
-                    return AlertDialog(title: Text("パスワードを変更しました"));
-                  },
-                );
-              },
-              child: Text("パスワードを忘れた方はこちら"),
+            trailing: Icon(Icons.keyboard_arrow_right),
+            onTap: (){},
+          ),ListTile(
+            // leading: FlutterLogo(size: 72.0),
+            leading: Icon(Icons.phone),
+            title: Text('佐藤 花子'),
+            subtitle: Text(
+              '090-1234-567',
             ),
-          ],
-        ),
+            trailing: Icon(Icons.keyboard_arrow_right),
+            onTap: (){},
+          ),
+        ],
       ),
     );
   }
