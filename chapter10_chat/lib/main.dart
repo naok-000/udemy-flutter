d3dc7debb5dbfa6c3a64cb5ab8b5f3d6e2929598
naok-000 <wt.94p.1978@s.thers.ac.jp>
2025-04-03 13:48:52 +0900

    チャット機能を実装

--- chapter10_chat/lib/main.dart
+++ chapter10_chat/lib/main.dart
@@ -1,6 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+Future<void> main() async {
+  // 非同期通信対応のmain
+  // Firebaseとの通信を行うため
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
@@ -34,28 +41,31 @@ class _MyHomePageState extends State<MyHomePage> {
 
   // エラー表示
   void showError(String message) {
-    showDialog(context: context, builder: (context){
-      return AlertDialog(
-        content: Text(message),
-      );
-    });
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(content: Text(message));
+      },
+    );
   }
 
   // 入室処理
   void enter() {
-    if(name.isEmpty){
+    if (name.isEmpty) {
       showError('あなたの名前を入力してください');
       return;
     }
-    if(room.isEmpty){
+    if (room.isEmpty) {
       showError('部屋名を入力してください。');
       return;
     }
     Navigator.push(
       context,
-      MaterialPageRoute(builder: (context){
-        return ChatPage(name: name, room: room);
-      })
+      MaterialPageRoute(
+        builder: (context) {
+          return ChatPage(name: name, room: room);
+        },
+      ),
     );
   }
 
@@ -106,8 +116,98 @@ class ChatPage extends StatefulWidget {
 }
 
 class _ChatPageState extends State<ChatPage> {
+  List<Item> items = [];
+  late FirebaseFirestore firestore;
+  late CollectionReference<Map<String, dynamic>> collection;
+  final TextEditingController textEditingController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    // firestoreのための処理
+    firestore = FirebaseFirestore.instance;
+    collection = firestore
+        .collection('rooms')
+        .doc(widget.room)
+        .collection('items');
+    watch();
+  }
+
+  // データ更新監視
+  Future<void> watch() async {
+    collection.snapshots().listen((event) {
+      if (mounted) {
+        // stateobjectが存在するか？
+        setState(() {
+          items = event.docs.reversed
+              .map(
+                (document) => Item.fromSnapshot(document.id, document.data()),
+              )
+              .toList(growable: false);
+        });
+      }
+    });
+  }
+
+  // 保存する
+  Future<void> save() async {
+    final now = DateTime.now();
+    await collection.doc(now.millisecondsSinceEpoch.toString()).set({
+      'date': Timestamp.fromDate(now),
+      'name': widget.name,
+      'text': textEditingController.text,
+    });
+    textEditingController.text = '';
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(title: Text(widget.room)));
+    return Scaffold(
+      appBar: AppBar(title: Text(widget.room)),
+      body: Column(
+        children: [
+          Expanded(
+            child: ListView.builder(
+              reverse: true,
+              itemBuilder: (context, index) {
+                final item = items[index];
+                return ListTile(
+                  subtitle: Text(item.name),
+                  title: Text(item.text),
+                );
+              },
+              itemCount: items.length,
+            ),
+          ),
+          SafeArea(
+            child: ListTile(
+              title: TextField(controller: textEditingController),
+              trailing: ElevatedButton(
+                onPressed: () {
+                  save();
+                },
+                child: Text('送信'),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class Item {
+  const Item({required this.id, required this.name, required this.text});
+
+  final String id;
+  final String name;
+  final String text;
+
+  factory Item.fromSnapshot(String id, Map<String, dynamic> document) {
+    return Item(
+      id: id,
+      name: document['name'].toString() ?? '',
+      text: document['text'].toString() ?? '',
+    );
   }
 }
